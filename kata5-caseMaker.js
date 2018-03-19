036971c0c8891fe998e9ef14e8c66e390973f822
Anthony Siu <anthony.sl.siu@gmail.com>
2018-03-19 16:31:06 -0700

    added kata5 exercise

---
+++ kata5-caseMaker.js
@@ -0,0 +1,10 @@
+//INSTRUCTIONS:
+//You'll be given a string which you'll have to convert to Camel Case. Return the resulting value from your function.
+
+var camelCase = function(input) {
+  
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
